5df7c1d1edbf997ab6de7de0651da4eb711c8ca9
Masahiro Yoshida <myoshida@aiming-inc.com>
2025-12-12 16:52:48 +0900

    add title

---
+++ client/Assets/Genie/Scripts/Title.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+public class Title : MonoBehaviour
+{
+    public struct Result
+    {
+        // 必要に応じてシーンの戻り値を定義
+    }
+
+    public static async UniTask<Result> StartAsync()
+    {
+        // Title GameObjectを取得
+        var titleObj = GameObject.Find("Title");
+        // 必要な初期化やUI表示などをここで行う
+        // ...
+        // 結果を返す
+        return new Result();
+    }
+}
+
