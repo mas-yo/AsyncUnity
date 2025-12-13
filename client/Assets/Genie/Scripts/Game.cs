dcf79c75c0b6379c15791bc52cdde82c55248855
Masahiro Yoshida <myoshida@aiming-inc.com>
2025-12-13 21:16:12 +0900

    add star ship on game scene

--- client/Assets/Genie/Scripts/Game.cs
+++ client/Assets/Genie/Scripts/Game.cs
@@ -4,20 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class Game : MonoBehaviour
 {
-    // シーンの戻り値
     public struct Result
     {
-        // 必要に応じてフィールドを追加
     }
 
-    // シーンの処理起点
     public static async UniTask<Result> StartAsync()
     {
-        SceneManager.LoadSceneAsync("GameScene", LoadSceneMode.Single);
-        // var startButton = GameObject.Find("StartButton").GetComponent<Button>();
-        // await startButton.OnClickAsync(token);
-        await UniTask.DelayFrame(60); // ダミー待機
+        await SceneManager.LoadSceneAsync("GameScene", LoadSceneMode.Single);
+        var prefab = await Resources.LoadAsync<GameObject>("StarShip/StarShip");
+        Object.Instantiate(prefab);
+        await UniTask.DelayFrame(600);
         return new Result();
     }
 }
-
