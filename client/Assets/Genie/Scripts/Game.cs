5914f1aa3066da988d6ff979b63ffa3ba334a29c
Masahiro Yoshida <myoshida@aiming-inc.com>
2025-12-15 19:09:28 +0900

    import 2D UI

--- client/Assets/Genie/Scripts/Game.cs
+++ client/Assets/Genie/Scripts/Game.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using Object = UnityEngine.Object;
 
 public class Game : MonoBehaviour
@@ -20,27 +21,58 @@ public class Game : MonoBehaviour
         var prefab = (GameObject)prefabRequest.asset;
         var robot = Object.Instantiate(prefab);
         var animator = robot.GetComponentInChildren<Animator>();
-        animator.Play("Run_guard_AR");
-        
+        animator.Play("Run_guard_AR");//Idle_gunMiddle_AR
+
 
         var moveAmount = 0.1f;
         while (true)
         {
-            await WaitAndDo(
+            var isEsc = await WaitAndDo(
                 token,
-                (() => Input.GetKey(KeyCode.W), () => robot.transform.position += Vector3.forward * moveAmount),
-                (() => Input.GetKey(KeyCode.S), () => robot.transform.position += Vector3.back * moveAmount),
-                (() => Input.GetKey(KeyCode.A), () => robot.transform.position += Vector3.left * moveAmount),
-                (() => Input.GetKey(KeyCode.D), () => robot.transform.position += Vector3.right * moveAmount),
-                (() => Input.GetKey(KeyCode.Escape), () => throw new OperationCanceledException())
+                (() => Input.GetKey(KeyCode.W), () =>
+                {
+                    robot.transform.position += Vector3.forward * moveAmount;
+                    animator.Play("Run_guard_AR");
+                    return false;
+                }),
+                (() => Input.GetKey(KeyCode.S), () =>
+                {
+                    robot.transform.position += Vector3.back * moveAmount;
+                    animator.Play("Run_guard_AR");
+                    return false;
+                }),
+                (() => Input.GetKey(KeyCode.A), () =>
+                {
+                    robot.transform.position += Vector3.left * moveAmount;
+                    animator.Play("Run_guard_AR");
+                    return false;
+                }),
+                (() => Input.GetKey(KeyCode.D), () =>
+                {
+                    robot.transform.position += Vector3.right * moveAmount;
+                    animator.Play("Run_guard_AR");
+                    return false;
+                }),
+                (() => Input.anyKey == false, () =>
+                {
+                    animator.Play("Idle_gunMiddle_AR");
+                    return false;
+                }),
+                (() => Input.GetKey(KeyCode.Escape), () => { return true; })
             );
+            if (isEsc)
+            {
+                break;
+            }
         }
+        
         return new Result();
+
     }
 
-    public static async UniTask WaitAndDo(CancellationToken token, params (Func<bool> predicate, Action action)[] conditionsAndActions)
+    public static async UniTask<T> WaitAndDo<T>(CancellationToken token, params (Func<bool> condition, Func<T> action)[] conditionsAndActions)
     {
-        var index = await UniTask.WhenAny(conditionsAndActions.Select(x => UniTask.WaitUntil(() => x.predicate(), cancellationToken: token)).ToArray());
-        conditionsAndActions[index].action();
+        var index = await UniTask.WhenAny(conditionsAndActions.Select(x => UniTask.WaitUntil(() => x.condition(), cancellationToken: token)).ToArray());
+        return conditionsAndActions[index].action();
     }
 }
